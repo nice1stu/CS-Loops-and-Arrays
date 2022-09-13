84a8fb7c97ff8a264981571834a3f8756e311bac
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-13 13:57:23 +0200

    P4 Done

--- P4ReturnKeyword/Program.cs
+++ P4ReturnKeyword/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void MyFunction()
+{
+    Console.WriteLine("What's your name?");
+    if (Console.ReadLine() == "Neo")
+    {
+        return;
+    }
+    Console.WriteLine("How old are you?");
+    string input = Console.ReadLine();
+    int InputInt = Convert.ToInt32(input);
+    
+    if (InputInt < 18)
+    {
+        return;
+    }
+    Console.WriteLine("Do you want to enter?");
+    if (Console.ReadLine() == "Yes")
+    {
+        return;
+    }
+    Console.WriteLine("Do you want to turn back??");
+    if (Console.ReadLine() == "No")
+    {
+        Console.WriteLine("Congratulations, you made it in.");
+    }
+}
+
+MyFunction();
