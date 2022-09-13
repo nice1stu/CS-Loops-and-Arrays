3d4c77427eebd3f09751bc0aff978901dc7920eb
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-13 11:29:29 +0200

    one scopes complete

--- P1Scopes/Program.cs
+++ P1Scopes/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+{
+    Console.WriteLine("Give me a number.");
+    //string input = Console.ReadLine();
+    int input = Convert.ToInt32(Console.ReadLine());
+    Console.Write(input);
+    Console.WriteLine(1);
+}
+{
+    Console.WriteLine("Give me another number.");
+    int input = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(input + 1);
+}
+{
+    Console.WriteLine("Give me another number.");
+    char input = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine(input ++ );
+}
