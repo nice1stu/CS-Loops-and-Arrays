cd491ce85089b835684142df66a6479968b3ceba
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-13 14:36:55 +0200

    1st draft P5

--- P5ReturnType/Program.cs
+++ P5ReturnType/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Rock-Paper-Scissors Mini-Game.
+
+string playerChoice;
+string aIChoice;
+
+
+void PlayerChoice()
+{
+    Console.WriteLine("Pick Rock, Paper or Scissors.");
+    string playerChoice = Console.ReadLine();
+
+    string playerChoice()
+    {
+        if (playerChoice == "Rock" || "Paper" || "Scissors")
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine("That's not a valid input.");
+        }
+    }
+}
