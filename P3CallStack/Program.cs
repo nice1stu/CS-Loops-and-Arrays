6ae6e10d7fbb72491d3b6021574fdae830a64f31
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-13 13:27:10 +0200

    P3 done

--- P3CallStack/Program.cs
+++ P3CallStack/Program.cs
@@ -2,5 +2,14 @@
 
 void Countdown()
 {
-    Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine(timer);
+        timer = timer - 1;
 }
+while (timer >= 0)
+{
+Countdown();
+}
+
+Console.WriteLine("Launch!");
+
