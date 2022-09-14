cc8175b4d5afd9de9c52cb9ed667eca6c7fdfe4a
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-14 04:37:09 +0200

    use command switch

--- P5ReturnType/Program.cs
+++ P5ReturnType/Program.cs
@@ -2,39 +2,100 @@
 
 string playerChoice;
 string aIChoice;
+string answer;
 Random random = new Random();
 bool playAgain = true;
 
-void PlayerChoice()
+
+while (playAgain)
 {
     playerChoice = "";
+    aIChoice = "";
+    
     while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors")
     {
         Console.WriteLine("Pick rock, paper or scissors.");
         playerChoice = Console.ReadLine();
         playerChoice = playerChoice.ToLower();
+        Console.WriteLine("That's not a valid input.");
     }
+    switch (random.Next(1, 4))
+    {
+        case 1:
+            aIChoice = "rock";
 
+            break;
+        case 2:
+            aIChoice = "paper";
+            
+            break;
+        case 3:
+            aIChoice = "scissors";
+            break;
+    }
+    Console.WriteLine("Player: " + playerChoice);
+    Console.WriteLine("HAL9000: " + aIChoice);
+
+    switch (playerChoice)
+    {
+        case "rock":
+            if (aIChoice == "rock")
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else if (aIChoice == "paper")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("You win!");   
+            }
+            break;
+        
+        case "paper":
+            if (aIChoice == "rock")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (aIChoice == "paper")
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");   
+            }
+            break;
+        case "scissors":
+            
+            if (aIChoice == "rock")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (aIChoice == "paper")
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");   
+            }
+            break;
+    }
+    Console.WriteLine("Would you like to play again?(y/n): ");
+    answer = Console.ReadLine();
+    answer = answer.ToLower();
+
+    if (answer == "y")
+    {
+        playAgain = true;
+        Console.Clear();
+    }
+    else
+    {
+        playAgain = false;
+        Console.Clear();
+    }
 }
-
-void AIChoice()
-{
-    int random 
-    if (random == 1)
-    {
-        aIChoice = "rock";
-    }
-    else if (random == 2)
-    {
-        aIChoice = "paper";
-    }
-    else if (random == 3)
-    {
-        aIChoice = "scissors";
-    }
-
-}
-PlayerChoice();
-Console.WriteLine(playerChoice);
-AIChoice();
-Console.WriteLine(aIChoice);
+Console.WriteLine("Thanks for playing");
