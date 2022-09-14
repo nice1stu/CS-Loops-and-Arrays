41db33ba76cc5cdc1f325fdda7b408413d72880f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-14 15:57:13 +0200

    changed but still not working

--- P6OneParameter/Program.cs
+++ P6OneParameter/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("What Fibonacci number would you like?");
 int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(fibonacci(n);
+//Console.WriteLine(fibonacci(n);
 
 int fibonacci(int n)
 {
@@ -9,8 +9,13 @@ int fibonacci(int n)
     {
         return 0;
     }
+
     if (n == 1)
     {
         return 1;
     }
-    return 
+
+    return = (return - 1) + (return -2);
+}
+
+Console.WriteLine(fibonacci);
