5a432a755e663aaeab464d06cbd20a38e8c5c41c
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-14 05:33:24 +0200

    try fibonacci equation

--- P6OneParameter/Program.cs
+++ P6OneParameter/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What Fibonacci number would you like?");
+int input = Convert.ToInt32(Console.ReadLine());
+int number = input - 1; //Need to decrement by 1 since we are starting from 0  
+int[] Fib = new int[number + 1];  
+Fib[0]= 0;  
+Fib[1]= 1;  
+for (int i = 2; i <= number;i++)  
+{  
+    Fib[i] = Fib[i - 2] + Fib[i - 1];  
+}  
+return Fib[number];
