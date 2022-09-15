14be03e0a5a6a6b70043e06e6fca2d3c69973bac
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-15 15:48:29 +0200

    print powers, but not working

--- P10ForLoops/Program.cs
+++ P10ForLoops/Program.cs
@@ -1,14 +1,23 @@
 ﻿int numberOfTimes = 13;
 int currentCount = 0;
 
-Console.WriteLine("Here is 13 random numbers (1-6):");
+Console.WriteLine("Here are 13 random numbers (1-6):");
 
 RandomGenLoop:
-
 for (currentCount = 0; currentCount < numberOfTimes; currentCount++)
 {
     Random random = new Random();
     int randomNum = random.Next(1, 6);
     Console.Write(randomNum);
-    
+}
+Console.WriteLine("");
+Console.WriteLine("Here's the power of 2's:");
+PowersLoop:
+for (currentCount = 0; currentCount < 11; currentCount++)
+{
+    double startNum = 0;
+    double answerNum;
+    answerNum = Math.Pow(2, startNum);
+    Console.Write($"{answerNum} - ");
+    startNum = answerNum;
 }
