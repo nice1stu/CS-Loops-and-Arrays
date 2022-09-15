b39967787c1f8a1d24029287c5535d73ac67a1bb
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-15 13:25:17 +0200

    try declare var at beginning

--- P9DoWhileLoops/Program.cs
+++ P9DoWhileLoops/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int min = 1;
+int max = 10;
+int guess;
+int number = 0;
+//AskNumberBetween();
+
+    int AskNumberBetween()
+    {
+        Random random = new Random();
+        int number = random.Next(min, max);
+        return number;
+    }
+Console.WriteLine(number);
+
+    do
+    {
+        Console.WriteLine($"Please give me a number between {min} and {max}");
+        string input = Console.ReadLine();
+        guess = Convert.ToInt32(input);
+    } while (AskNumberBetween() != guess);
+
+    Console.WriteLine($"You gave the number {guess}");
