6564919aeb1bd9f70a838efd77ef88617861fa55
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-15 05:17:53 +0200

    returns nothing still

--- P6OneParameter/Program.cs
+++ P6OneParameter/Program.cs
@@ -1,11 +1,14 @@
-﻿/*Console.WriteLine("What Fibonacci number would you like?");
+﻿Console.WriteLine("What Fibonacci number would you like?");
 string input = Console.ReadLine();
 int n = Convert.ToInt32(input);
-fibonacci((int n);
+
 int fibonacci(int n)
 {
-    n = 7;
-    if ((n == 0 || n == 1))
+    if (n == 0)
+    {
+        return n;
+    }
+    if (n == 1)
     {
         return n;
     }
@@ -13,22 +16,5 @@ int fibonacci(int n)
         return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-if (n == 1)
-{
-    return 1;
-}
-
 return n;
     
-
-Console.WriteLine(fibonacci);*/
-int n;
-int f(int n)
-{
-    if (n == 0 || n == 1)
-        return n;
-    else
-        return f(n - 1) + f(n - 2);
-}
-    
-    Console.WriteLine(f);
