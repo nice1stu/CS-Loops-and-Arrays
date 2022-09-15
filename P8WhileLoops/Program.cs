60c4891dcde748ad6585b6217a4fec7044fd716c
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-15 10:07:01 +0200

    done P8whileloops

--- P8WhileLoops/Program.cs
+++ P8WhileLoops/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int startMatches = 10;
+int remainingMatches = startMatches;
+
+Console.WriteLine("There are 10 Matches.");
+
+
+while (remainingMatches > 0)
+{
+    Console.WriteLine("How many do you want to draw?");
+    string choice = Console.ReadLine();
+    int choiceI = Convert.ToInt32(choice);
+    remainingMatches = startMatches - choiceI;
+    Console.WriteLine($"There are {remainingMatches} matches.");
+    startMatches = remainingMatches;
+}
+Console.WriteLine("Game Over");
