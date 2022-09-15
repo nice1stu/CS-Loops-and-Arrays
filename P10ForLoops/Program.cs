eda0bef728ca397a68b9879d389393e55e1797f4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-15 15:04:46 +0200

    generate 13 random numbers

--- P10ForLoops/Program.cs
+++ P10ForLoops/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberOfTimes = 13;
+int currentCount = 0;
+
+Console.WriteLine("Here is 13 random numbers (1-6):");
+
+RandomGenLoop:
+
+for (currentCount = 0; currentCount < numberOfTimes; currentCount++)
+{
+    Random random = new Random();
+    int randomNum = random.Next(1, 6);
+    Console.Write(randomNum);
+    
+}
