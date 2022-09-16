71a2c44ea74312eb3f79f6102bbf01e7e58f6db2
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-16 21:45:45 +0200

    P16 working

--- P16Arrays/Program.cs
+++ P16Arrays/Program.cs
@@ -1,15 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
-int result;
-int count = 0;
+﻿Console.WriteLine("I will roll 10.000 numbers between 0 and 10:");
 
+Random result = new Random();
 
+//Fill array with random numbers
+int[] table = new int[10000];
+for (int count = 0; count < table.Length; count++)
+    table[count] = result.Next(0, 11);
 
-// create random table of numbers between 0 - 10
-int[] randomChart = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//Count how many times a number occurs
+int[] numberCounts = new int[11];
+for (int count = 0; count < table.Length; count++)
+    numberCounts[table[count]]++;
 
-//generate random number;
-Random randomRoll = new Random();
-result = randomRoll.Next(0, 10);
-
-//rolled result +1 to the count
-Array[result] = count++;
+//Print the count of the numbers
+for (int count = 0; count < numberCounts.Length; count++)
+    Console.WriteLine($"I rolled {count}'s a total of {numberCounts[count]} times");
