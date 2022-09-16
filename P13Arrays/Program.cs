fac5393264f860011b8987cb604bb57490c0a719
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-16 21:56:49 +0200

    clarified

--- P13Arrays/Program.cs
+++ P13Arrays/Program.cs
@@ -2,16 +2,16 @@
 
 Random result = new Random();
 
-//Fill array with random numbers
+//Fill table with 10000 random numbers
 int[] table = new int[10000];
 for (int count = 0; count < table.Length; count++)
     table[count] = result.Next(0, 11);
 
-//Count how many times a number occurs
+//Count how many times each result occurs
 int[] numberCounts = new int[11];
 for (int count = 0; count < table.Length; count++)
     numberCounts[table[count]]++;
 
-//Print the count of the numbers
+//Print the count of the number of times each result occured
 for (int count = 0; count < numberCounts.Length; count++)
     Console.WriteLine($"I rolled {count}'s a total of {numberCounts[count]} times");
