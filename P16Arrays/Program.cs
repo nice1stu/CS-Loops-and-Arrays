e14514d0ec947677b5b8438b421459fcf3c4e4b8
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-16 20:54:42 +0200

    Add initial conditions

--- P16Arrays/Program.cs
+++ P16Arrays/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+int result;
+int count = 0;
+
+
+
+// create random table of numbers between 0 - 10
+int[] randomChart = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//generate random number;
+Random randomRoll = new Random();
+result = randomRoll.Next(0, 10);
+
+//rolled result +1 to the count
+Array[result] = count++;
