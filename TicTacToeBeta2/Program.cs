f152b8ef6041117a8e72279afee95ee4bcb60e17
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-18 01:24:14 +0200

    clean up code

--- TicTacToeBeta2/Program.cs
+++ TicTacToeBeta2/Program.cs
@@ -1,18 +1,16 @@
 ﻿Console.WriteLine("Tic Tac Toe Beta 2");
 Main(args);
-void Main(string[] args)
+void Main(string[]args)
 {
     int gameStatus = 0;
     int currentPlayer = -1;
     char[] gameMarkers = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
     
     do
     {
         Console.Clear();
         currentPlayer = GetNextPlayer(currentPlayer);
-        
-        
+
         Instructions(currentPlayer);
         DrawGameBoard(gameMarkers);
 
@@ -20,8 +18,8 @@ void Main(string[] args)
 
         gameStatus = CheckWinner(gameMarkers);
         
-        
     } while (gameStatus.Equals(0));
+    Console.Clear();
     Instructions(currentPlayer);
     DrawGameBoard(gameMarkers);
     if (gameStatus.Equals(1))
@@ -34,26 +32,27 @@ void Main(string[] args)
         Console.WriteLine("The game is a draw");
     }
 }
-
+static void Instructions(int playerNumber)
+{
+    Console.WriteLine("--Welcome to Tic Tac Toe--");
+    Console.WriteLine("Player 1 : X");
+    Console.WriteLine("Player 2 : O");
+    Console.WriteLine();
+    Console.WriteLine($"Player {playerNumber} to move, select 1 - 9 on the gameboard");
+    Console.WriteLine();
+}
 static int CheckWinner(char[] gameMarkers)
 {
-    //3.1 after each turn determine if won
-    //3.2 if no winner back to step 1
-    
-    //3.3 if all markers placed and no winner, its draw stop game
     if (IsGameDraw(gameMarkers))
     {
         return 2;
     }
-
-    //3.4 if winner, announce who won and stop game
     if (IsGameWinner(gameMarkers))
     {
         return 1;
     }
     return 0;
 }
-
 static bool IsGameDraw(char[] gameMarkers)
 {
     return gameMarkers[0] != '1' &&
@@ -66,7 +65,6 @@ static bool IsGameDraw(char[] gameMarkers)
            gameMarkers[7] != '8' &&
            gameMarkers[8] != '9';
 }
-    
 static bool IsGameWinner(char[] gameMarkers)
 {
     if (IsGameMarkersTheSame(gameMarkers, 0, 1, 2))
@@ -104,12 +102,10 @@ static bool IsGameWinner(char[] gameMarkers)
 
     return false;
 }
-
 static bool IsGameMarkersTheSame(char[] testGameMarkers, int pos1, int pos2, int pos3)
 {
     return testGameMarkers[pos1].Equals(testGameMarkers[pos2]) && testGameMarkers[pos2].Equals(testGameMarkers[pos3]);
 }
-
 void GameEngine(char[] gameMarkers, int currentPlayer)
 {
     bool notValidMove = true;
@@ -152,7 +148,6 @@ void GameEngine(char[] gameMarkers, int currentPlayer)
     } while (notValidMove);
 
 }
-
 static char GetPlayerMarker(int player)
 {
     if (player % 2 == 0)
@@ -162,35 +157,14 @@ static char GetPlayerMarker(int player)
 
     return 'X';
 }
-
-static void Instructions(int playerNumber)
-{
-    //1.  Provide instructions
-    //1.1 greeting
-    Console.WriteLine("--Welcome to Tic Tac Toe--");
-    //1.2 Display Player symbols (X / O)
-    Console.WriteLine("Player 1 : X");
-    Console.WriteLine("Player 2 : O");
-    Console.WriteLine();
-    //1.3 Who's turn is it
-    //1.4 instruct player to enter number 1 - 9
-    Console.WriteLine($"Player {playerNumber} to move, select 1 - 9 on the gameboard");
-    Console.WriteLine();
-}
-
 static void DrawGameBoard(char[] gameMarkers)
 {
-    //2.  Draw game board
-    //2.1 Game will have 3 row & 3 columns numbered 1 - 9
-
     Console.WriteLine($" {gameMarkers[6]} | {gameMarkers[7]} | {gameMarkers[8]} ");
     Console.WriteLine("---+---+---");
     Console.WriteLine($" {gameMarkers[3]} | {gameMarkers[4]} | {gameMarkers[5]} ");
     Console.WriteLine("---+---+---");
     Console.WriteLine($" {gameMarkers[0]} | {gameMarkers[1]} | {gameMarkers[2]} ");
-
 }
-
 static int GetNextPlayer(int player)
 {
     if (player.Equals(1))
