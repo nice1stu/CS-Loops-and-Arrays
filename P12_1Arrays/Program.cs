31f85338bf43305719e2edbc97e9cde970867ce6
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-19 21:41:06 +0200

    create P12_1Arrays

---
+++ P12_1Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("How many numbers do you want to give me?");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("OK, give me the numbers");
+int[] giveMeNumbers = new int[numbers];
+for (int i = 0; i < numbers; i++)
+{
+    int userAnswers = Convert.ToInt32(Console.ReadLine());
+    giveMeNumbers[i] = userAnswers;
+}
