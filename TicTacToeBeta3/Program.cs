90f761303c2055d18b0d231458abb83cfe46aa8d
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-20 01:16:57 +0200

    test player input array

--- TicTacToeBeta3/Program.cs
+++ TicTacToeBeta3/Program.cs
@@ -5,7 +5,7 @@ int numPlayers;
 
 //--Arrays--
 string[] playerNames = new string[4]; //Store Player Names
-
+string[] playerSymbol = { "X", "O" }; //Player Symbols to use. PLayer 1 [0] = X, Player 2 & AI [1,2,3] = O
 string[,] cellSymbol = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } }; //Numbers represent squares to input. Represent numPad
 //locations (2,0),(2,1),(2,2) | (1,0),(1,1),(1,2) | (0,0),(0,1),(0,2)
 int[,] CellValue = { { 5, 5, 5 }, { 5, 5, 5 }, { 5, 5, 5 } }; //Value of each cell
@@ -59,7 +59,28 @@ void DrawBoard()
     Console.WriteLine("- + - + - ");
     Console.WriteLine($"{cellSymbol[0,0]} | {cellSymbol[0,1]} | {cellSymbol[0,2]}");
 }
-//Player input
+
+//Player moves
+
+Console.WriteLine($"{playerNames [0]}  choose the Number of the square you want to place your X");
+string playerChoice = Console.ReadLine();
+//CheckValidMove
+//CheckWin
+//CheckDraw
+for (int i = 0; i < cellSymbol.Length; i++)
+{
+    for (int j = 0; j < cellSymbol.Length; j++)
+    {
+        if (playerChoice == cellSymbol[i,j])
+        {
+            cellSymbol[i,j] = playerSymbol[0];
+        }
+    }
+
+}
+DrawBoard();
+
+
 //check valid move
 //check win
 //check draw
