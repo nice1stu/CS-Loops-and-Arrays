e1247ac6408e967a1e6f8aa478671cd8611a007e
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-21 23:57:50 +0200

    add invalid input check

--- TicTacToeBeta4/Program.cs
+++ TicTacToeBeta4/Program.cs
@@ -3,35 +3,62 @@
 
 //declare & initialize
 char[,] gameBoard = new Char[3,3];
-char[] playerSymbol = new char[2]{'X','O'};
-bool invalidMove = false; ;
+char[] playerSymbol = {'X','O'}; //Player Symbols to use. PLayer 1 [0] = X, Player 2 & AI [1,2,3] = O
+bool invalidMove = false;
 int gameMarker = 0;
+string[] playerName = new string[4]; //Store Player Names
+int numPlayers;
 
-while (!invalidMove) // invalid move if square occupied
+
+
+SetUp();
+
+//Setup Game
+void SetUp()
 {
-    PlayerInput:
-    Console.WriteLine("Please enter where you want to play along X axis");
-    int playerChoiceX = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Please enter where you want to play along Y axis");
-    int playerChoiceY = Convert.ToInt32(Console.ReadLine());
-    
-    if (gameBoard [playerChoiceX,playerChoiceY] == 'X' || gameBoard [playerChoiceX,playerChoiceY] == 'O')
+    Console.Clear();
+    Console.WriteLine("-- Welcome to Tic Tac Toe --");
+
+    //Number of players
+    Console.WriteLine("Please enter the number of player 1 or 2");
+    numPlayers = Convert.ToInt32(Console.ReadLine());
+    numPlayers = (int)Math.Clamp((double)numPlayers, 1, 2);
+    if (numPlayers == 2)
     {
-        Console.WriteLine("Cell has been played, please select another cell");
-        goto PlayerInput;
+        //Players enter name
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"Player {i + 1}, please enter your name");
+            string playerAnswer = Console.ReadLine();
+            playerName[i] = playerAnswer;
+        }
+
+/*
     }
     else
     {
-        gameBoard[playerChoiceX, playerChoiceY] = playerSymbol[0];
+        Console.WriteLine("Player 1, please enter your name");
+        string playerAnswer = Console.ReadLine();
+        playerName[0] = playerAnswer;
+        Console.WriteLine("Player 2 is WALL-E");
+        playerName[2] = "WALL-E";
     }
-    DrawBoard();
-
-
+*/
+        Console.WriteLine(" ");
+        Console.WriteLine("Lets Play !");
+        Console.WriteLine(" ");
+    }
 }
 
 
-gameBoard[1,1] = 'X';//Test !!!!!
-DrawBoard();
+//game mechanic !
+while (true) // invalid move if square occupied
+{
+    PlayerTurn1();
+    DrawBoard();
+    PlayerTurn2();
+    DrawBoard();
+}
 
 //Drawboard
 void DrawBoard()
@@ -44,4 +71,62 @@ void DrawBoard()
     Console.WriteLine("    - + - + - ");
     Console.WriteLine($"[0] {gameBoard[0,0]} | {gameBoard[1,0]} | {gameBoard[2,0]}");
     Console.WriteLine("   [0] [1] [2]");
+}
+
+void PlayerTurn1()//make into 1 method
+{
+    PlayerInput:
+    Console.Write($"{playerName[0]} ");
+    Console.WriteLine("enter where you want to play along X axis");
+    int playerChoiceX = Convert.ToInt32(Console.ReadLine());
+    if (playerChoiceX !=0 && playerChoiceX !=1 && playerChoiceX !=2)
+    {
+        Console.WriteLine("invalid input");
+        goto PlayerInput;
+    }
+    Console.WriteLine("enter where you want to play along Y axis");
+    int playerChoiceY = Convert.ToInt32(Console.ReadLine());
+    if (playerChoiceY !=0 && playerChoiceY !=1 && playerChoiceY !=2)
+    {
+        Console.WriteLine("invalid input");
+        goto PlayerInput;
+    }
+
+    if (gameBoard [playerChoiceX,playerChoiceY] == 'X' || gameBoard [playerChoiceX,playerChoiceY] == 'O')
+    {
+        Console.WriteLine("Cell has been played, please select another cell");
+        goto PlayerInput;
+    }
+    else
+    {
+        gameBoard[playerChoiceX, playerChoiceY] = playerSymbol[0];
+    }
+}
+
+void PlayerTurn2()//make into 1 method
+{
+    PlayerInput:
+    Console.Write($"{playerName[1]} ");
+    Console.WriteLine("enter where you want to play along X axis");
+    int playerChoiceX = Convert.ToInt32(Console.ReadLine());
+    if (playerChoiceX !=0 && playerChoiceX !=1 && playerChoiceX !=2)
+    {
+        Console.WriteLine("invalid input");
+        goto PlayerInput;
+    }
+    Console.WriteLine("enter where you want to play along Y axis");
+    int playerChoiceY = Convert.ToInt32(Console.ReadLine());
+    if (playerChoiceY !=0 && playerChoiceY !=1 && playerChoiceY !=2)
+    {
+        Console.WriteLine("invalid input");
+        goto PlayerInput;
+    }
+    if (gameBoard [playerChoiceX,playerChoiceY] == 'X' || gameBoard [playerChoiceX,playerChoiceY] == 'O')
+    {
+        Console.WriteLine("Cell has been played, please select another cell");
+        goto PlayerInput;
+    }
+    {
+        gameBoard[playerChoiceX, playerChoiceY] = playerSymbol[1];
+    }
 }
