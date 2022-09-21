a180a11e7b83c6c2d3e269e04224056c923c8a1e
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-21 16:22:33 +0200

    create instruction field comments and 5x5 array

--- P12_3Arrays/Program.cs
+++ P12_3Arrays/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//We'll make a text-driven Painting Tool!
+
+//Create a two-dimensional array as the canvas. We want to be able to store letters in that array. What type do you need? An array of size 5x5 should be enough for this demo.
+//Repeatedly:
+//Print all the contents of the two-dimensional array to the user by printing the complete array to the user. But also print the coordinates, so users understand which is the x and y coordinate and what values they have.
+//Ask the user for a x coordinate
+//Ask the user for a y coordinate
+//Ask the user for a symbol
+//Store the symbol at the correct index of the two dimensional array
+
+char[,] paintToolArray = new char[5,5];
