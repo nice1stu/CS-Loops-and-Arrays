88d1cfbe65e0c2512b995fd9b08860e5e3799b90
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-21 22:50:56 +0200

    Have working invalid move !!!

--- TicTacToeBeta4/Program.cs
+++ TicTacToeBeta4/Program.cs
@@ -1,28 +1,47 @@
 ﻿//TicTacToe Beta4
-bool validMove = true;
-string player1Symbol = "X";
-string player2Symbol = "O";
-string[] cellSymbol = { "1", "X", "3", "4", "5", "6", "7", "8", "9" };
-string playerChoice;
+//Start again with 2D arrays
 
-PlayerTurn();
+//declare & initialize
+char[,] gameBoard = new Char[3,3];
+char[] playerSymbol = new char[2]{'X','O'};
+bool invalidMove = false; ;
+int gameMarker = 0;
 
-void PlayerTurn()
+while (!invalidMove) // invalid move if square occupied
 {
-    Console.WriteLine("Select the number where you want to play X");
-    playerChoice = Console.ReadLine();
-    while (validMove)
+    PlayerInput:
+    Console.WriteLine("Please enter where you want to play along X axis");
+    int playerChoiceX = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please enter where you want to play along Y axis");
+    int playerChoiceY = Convert.ToInt32(Console.ReadLine());
+    
+    if (gameBoard [playerChoiceX,playerChoiceY] == 'X' || gameBoard [playerChoiceX,playerChoiceY] == 'O')
     {
-        for (int j = 0; j < cellSymbol.Length; j++)
-        {
-            if (player1Symbol == cellSymbol [j])
-            {
-                validMove = false;
-                Console.WriteLine("Move invalid, please select another position");
-                PlayerTurn();
-            }
-        }
-
-
+        Console.WriteLine("Cell has been played, please select another cell");
+        goto PlayerInput;
     }
+    else
+    {
+        gameBoard[playerChoiceX, playerChoiceY] = playerSymbol[0];
+    }
+    DrawBoard();
+
+
+}
+
+
+gameBoard[1,1] = 'X';//Test !!!!!
+DrawBoard();
+
+//Drawboard
+void DrawBoard()
+{
+    Console.Clear();
+    Console.WriteLine();
+    Console.WriteLine($"[2] {gameBoard[0,2]} | {gameBoard[1,2]} | {gameBoard[2,2]}");
+    Console.WriteLine("    - + - + -");
+    Console.WriteLine($"[1] {gameBoard[0,1]} | {gameBoard[1,1]} | {gameBoard[2,1]}");
+    Console.WriteLine("    - + - + - ");
+    Console.WriteLine($"[0] {gameBoard[0,0]} | {gameBoard[1,0]} | {gameBoard[2,0]}");
+    Console.WriteLine("   [0] [1] [2]");
 }
