cadd271e3a019b89e0c31124b4bb268666ffd2f7
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-21 21:55:00 +0200

    P12_3Arrays Done

--- P12_3Arrays/Program.cs
+++ P12_3Arrays/Program.cs
@@ -1,30 +1,31 @@
 ﻿//We'll make a text-driven Painting Tool!
+// give X, Y, symbol as 2,3,O / 1,4,E / 2,4,Y
 
-//Create a two-dimensional array as the canvas. We want to be able to store letters in that array. What type do you need? An array of size 5x5 should be enough for this demo.
-//Repeatedly:
-//Print all the contents of the two-dimensional array to the user by printing the complete array to the user. But also print the coordinates, so users understand which is the x and y coordinate and what values they have.
-//Ask the user for a x coordinate
-//Ask the user for a y coordinate
-//Ask the user for a symbol
-//Store the symbol at the correct index of the two dimensional array
-
+//delcare & initialise
 string[,] paintToolArray = new String[5,5];
 int getXcordinate;
 int getYcordinate;
 string getsymbol;
-
 
 //initialize all values of array
 for (int y = 0; y < 5; y++)
 {
     for (int x = 0; x < 5; x++)
     {
-        paintToolArray[x, y] = "!";
+        paintToolArray[x, y] = " ";
     }
 }
 
-UserInput();
-PrintXYGrid();
+P12_3Arrays();
+
+void P12_3Arrays()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        UserInput();
+        PrintXYGrid();
+    }
+}
 
 //print paintToolArray
 void PrintXYGrid()
@@ -32,14 +33,13 @@ void PrintXYGrid()
     Console.WriteLine("Y");
     for (int y = 0; y < 5; y++)
     {
-
         Console.Write($"{4-y}");
         for (int x = 0; x < 5; x++)
         {
             Console.Write($"{paintToolArray[x,(4-y)]}");
         }
         Console.WriteLine();
-        }
+    }
     Console.Write("\\");
     for (int x = 0; x < 5; x++)
     {
