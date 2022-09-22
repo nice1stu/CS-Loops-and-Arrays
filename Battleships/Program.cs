cfd492c2aef71159be8c21fb132c18cbcc4ab189
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-22 03:40:37 +0200

    define some arrays

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Lets try to make Battleship");
+
+//declare arrays & variables
+char[,] player1FleetMap = new char[9, 9];
+char[,] player1AttackMap = new char[9, 9];
+char[,] player2FleetMap = new char[9, 9];
+char[,] player2AttackMap = new char[9, 9];
+
+for (int x = 0; x < player1AttackMap.Length; x++)
+{
+    for (int y = 0; y < player1AttackMap.Length; y++)
+    {
+        player1AttackMap[x, y] = 'O';
+    }
+}
+
+//draw maps
+void Player1FleetMap()
+{
+    for (int x = 0; x < player1AttackMap.Length; x++)
+    {
+        for (int y = 0; y < player1AttackMap.Length; y++)
+        {
+            Console.Write(player1AttackMap[x,y]);
+        }
+        Console.WriteLine();
+    }
+}
